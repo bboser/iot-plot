3299975d6e318886a6eb8a488b0ebfc9d2dad045
Bernhard Boser <boser@berkeley.edu>
2018-01-06 19:39:04 -0800

    removed download url

--- setup.py
+++ setup.py
@@ -8,13 +8,12 @@ if sys.version_info < (3,4):
 setup(
   name = 'iot_plot',
   packages = ['iot_plot'],
-  version = "0.4",
+  version = "0.5",
   description = 'Remote plotting library',
   license = 'MIT',
   author = 'Bernhard Boser',
   author_email = 'boser@berkeley.edu',
   url = 'https://github.com/bboser/iot-plot',
-  download_url = 'https://github.com/bboser/iot-plot/archive/0.1.tar.gz',
   keywords = ['plotting', 'micropython', 'MQTT'],
   classifiers = [
       'Development Status :: 3 - Alpha',
