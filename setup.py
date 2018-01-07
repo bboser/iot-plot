bd0be8e44fcb0e97021475b334f14781eb41c13b
Bernhard Boser <boser@berkeley.edu>
2018-01-06 19:31:03 -0800

    upped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (3,4):
 setup(
   name = 'iot_plot',
   packages = ['iot_plot'],
-  version = "0.3",
+  version = "0.4",
   description = 'Remote plotting library',
   license = 'MIT',
   author = 'Bernhard Boser',
