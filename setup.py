aae8fa878248c7e6a13c185739501462d575b7c7
Bernhard Boser <boser@berkeley.edu>
2018-01-06 19:45:52 -0800

    added long_description

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ if sys.version_info < (3,4):
 setup(
   name = 'iot_plot',
   packages = ['iot_plot'],
-  version = "0.5",
+  version = "0.6",
   description = 'Remote plotting library',
+  long_description = 'See documentation at https://github.com/bboser/iot-plot'
   license = 'MIT',
   author = 'Bernhard Boser',
   author_email = 'boser@berkeley.edu',
